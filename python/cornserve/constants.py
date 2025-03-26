8a68f1d22b840a66f01e7c2d6c930a609f1fc86b
Jeff Ma <jeffjma@umich.edu>
2025-03-26 15:36:53 -0400

    update otel endpoint var name

--- python/cornserve/constants.py
+++ python/cornserve/constants.py
@@ -6,11 +6,10 @@ K8S_SIDECAR_SERVICE_NAME = "sidecar"
 K8S_TASK_DISPATCHER_HTTP_URL = "task-dispatcher:8000"
 K8S_TASK_DISPATCHER_GRPC_URL = "task-dispatcher:50051"
 K8S_RESOURCE_MANAGER_GRPC_URL = "resource-manager:50051"
+K8S_OTEL_GRPC_URL = "http://jaeger-collector.cornserve:4317"
 
 CONTAINER_IMAGE_TASK_MANAGER = "cornserve/task-manager:latest"
 CONTAINER_IMAGE_ERIC = "cornserve/eric:latest"
 CONTAINER_IMAGE_VLLM = "cornserve/vllm:latest"
 
 VOLUME_HF_CACHE = "/data/hfcache"
-
-OTEL_COLLECTOR_URL = "http://jaeger-collector.cornserve:4317"
--- python/cornserve/tracing.py
+++ python/cornserve/tracing.py
@@ -1,19 +1,19 @@
 """OpenTelemetry configuration for the cornserve services."""
 
 from opentelemetry import trace
-from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace.export import BatchSpanProcessor
 from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
 from opentelemetry.sdk.resources import Resource
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
 
-from cornserve.constants import OTEL_COLLECTOR_URL
+from cornserve.constants import K8S_OTEL_GRPC_URL
 
 
 def configure_otel(name: str) -> None:
     """Configure OpenTelemetry for the given service name."""
     resource = Resource.create({"service.name": name})
     provider = TracerProvider(resource=resource)
-    exporter = OTLPSpanExporter(endpoint=OTEL_COLLECTOR_URL)
+    exporter = OTLPSpanExporter(endpoint=K8S_OTEL_GRPC_URL)
     processor = BatchSpanProcessor(exporter)
     provider.add_span_processor(processor)
     trace.set_tracer_provider(provider)
