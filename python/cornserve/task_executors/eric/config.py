82ce441257d3dc569fd54b00af1bafdbc5dd9bb3
Jae-Won Chung <jwnchung@umich.edu>
2025-07-05 03:38:26 -0400

    Impl

--- python/cornserve/task_executors/eric/config.py
+++ python/cornserve/task_executors/eric/config.py
@@ -63,6 +63,9 @@ class ServerConfig(BaseModel):
     # Port to bind to
     port: PositiveInt = 8000
 
+    # Maximum batch size to run the encoder with
+    max_batch_size: PositiveInt = 4
+
 
 class ImageDataConfig(BaseModel):
     """Configuration related to downloading and processing image data."""
