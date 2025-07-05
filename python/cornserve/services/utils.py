5817e385f64617f319ac1173fe984564a687050f
Jae-Won Chung <jwnchung@umich.edu>
2025-07-05 04:09:26 -0400

    Refine and add tests

--- python/cornserve/services/utils.py
+++ python/cornserve/services/utils.py
@@ -14,9 +14,21 @@ def to_strict_k8s_name(name: str) -> str:
 
     Ref: https://kubernetes.io/docs/concepts/overview/working-with-objects/names
     """
+    # Only lowercase alphanumeric characters and '-'
     name = name.lower()
     name = "".join(c if c.isalnum() or c == "-" else "-" for c in name)
+
+    # Ensure length
+    name = name[:63]
+
+    # Starts and ends with an alphanumeric character
     name = name.strip("-")
+
+    # Ensure it starts with an alphabetic character
+    while name and name[0].isnumeric():
+        name = name[1:]
+
     if not name:
         raise ValueError("Name cannot be empty after normalization.")
-    return name[:63]
+
+    return name
