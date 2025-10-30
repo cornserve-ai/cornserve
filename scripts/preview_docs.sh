94be7c750d294b290029ce79a75b85696a54a47e
Jae-Won Chung <jwnchung@umich.edu>
2025-10-29 23:46:07 -0400

    Fix doc preview script error message

--- scripts/preview_docs.sh
+++ scripts/preview_docs.sh
@@ -8,7 +8,7 @@ uv pip list | grep mkdocs-material 2>&1 >/dev/null
 
 if [[ ! $? -eq 0 ]]; then
   echo "Run the following to install documentation build dependnecies:"
-  echo "    pip install '.[docs]'"
+  echo "    uv pip install -r docs/requirements.txt"
   exit 1
 fi
 
